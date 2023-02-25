c0ce2a409cb34ed09ae57042214c6b61bfcebfc9
sairamgunner <sairamgunner@gmail.com>
2023-02-25 17:19:26 -0500

    Added comments

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 console.log('hi');
-
+// const element = React.createElement('div', null, 'React element!');
 const element = <div>React element with JSX!</div>
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
